d446acc49bfae3d66ddadb3b444270042dcb76f2
anthonny340 <tonysolorzano880@gmail.com>
2025-09-19 14:05:17 -0500

    feat(screens): nuevas pantallas agregadas
    Tambien se creo el archivo de barril screens

---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cubit Counter'),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+      ),
+      body: Center(child: const Text('Counter Value: XX')),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            heroTag: '1',
+            child: const Text('+3'),
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            heroTag: '2',
+            child: const Text('+2'),
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            heroTag: '3',
+            child: const Text('+1'),
+          ),
+          // FloatingActionButton(onPressed: () {}, heroTag: '4'),
+        ],
+      ),
+    );
+  }
+}
