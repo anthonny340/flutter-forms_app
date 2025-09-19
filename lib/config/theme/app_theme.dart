314a0996710ac96c55fc067ae941bd894df83d3c
anthonny340 <tonysolorzano880@gmail.com>
2025-09-19 13:44:31 -0500

    app_theme: Modificado

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   ThemeData getTheme() {
-    return ThemeData(useMaterial3: true);
+    const seedColor = Colors.deepOrange;
+
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: seedColor,
+      listTileTheme: const ListTileThemeData(iconColor: seedColor),
+    );
   }
 }
